c0a4101a0d080d39af1f66ef02b93af2c7fcc953
92hoy <ho_9209@naver.com>
2020-04-01 10:28:14 +0900

    board_delete

--- pages/board.js
+++ pages/board.js
@@ -7,8 +7,8 @@ import BoardForm from '../components/Board/BoardForm'
 class Board extends React.Component {
     state = {
         rowsSelected: []
-      };
-    
+    };
+
 
     static async getInitialProps({ req }) {
 
@@ -38,64 +38,74 @@ class Board extends React.Component {
             });
     }
 
-
+    handleDelete = (data) => {
+        console.log("--=" + data)
+        axios({
+            method: 'put',
+            url: 'http://127.0.0.1:8111/v1/delete_board',
+            data: {
+                id: data
+            }
+        })
+            .then(function (response) {
+                window.location.reload();
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
     render() {
 
         const { data } = this.props;
-        const columns = ["id", "title", "context", "regist_date"];
+        const columns = [{
+            name: "id",
+            options: {
+                // display: "excluded",
+                // filterType: 'checkbox',
+                // download: false,
+            },
+        },
+            "title", "context", "regist_date"];
         const options = {
             filter: true,
-      selectableRows: 'multiple',
-      selectableRowsOnClick: true,
-      filterType: 'dropdown',
-      responsive: 'stacked',
-      rowsPerPage: 10,
-      rowsSelected: this.state.rowsSelected,
-      onRowsSelect: (rowsSelected, allRows) => {
-        console.log(rowsSelected, allRows);
-        this.setState({ rowsSelected: allRows.map(row => row.dataIndex) });
-      },
-      onRowsDelete: (rowsDeleted) => {
-        if (rowsDeleted.data[0].dataIndex === 0) {
-          window.alert('Can\'t delete this!');
-          return false;
+            filterType: 'checkbox',
+            selectableRowsOnClick: true,
+            selectableRows: true,
+            responsive: 'stacked',
+            rowsPerPage: 10,
+            rowsSelected: this.state.rowsSelected,
+            onRowsSelect: (rowsSelected, allRows) => {
+                this.setState({ rowsSelected: allRows.map(row => row.dataIndex) }); ``
+                console.log("onrowselect", rowsSelected);
+                console.log(allRows.map(row => row[1]))
+            },
+            onRowsDelete: (rowsDeleted) => {
+                // if (rowsDeleted.data[0].dataIndex === 0) {
+                //     window.alert('Can\'t delete this!');
+                //     return false;
+                // };
+                let test_list = [];
+                console.log(rowsDeleted);
+                for (let i = 0; i < rowsDeleted.data.length; i++) {
+                    test_list.push(rowsDeleted.data[i]['data']);
+                    console.log(test_list);
+                    console.log("test", rowsDeleted)
+                }
+                // this.handleDelete(test_list)
+                // console.log("-----",rowsDeleted.data, "were deleted!");
+            },
+
+            onCellClick: (cellData, cellMeta) => {
+                console.log("1", cellData);
+            },
+            isRowSelectable: (dataIndex, selectedRows) => {
+                //prevents selection of any additional row after the third
+                if (selectedRows.data.length > 999 && selectedRows.data.filter(d => d.dataIndex === dataIndex).length === 0) return false;
+                //prevents selection of row with title "Attorney"
+                return data[dataIndex][1] != "Attorney";
+            },
+            selectableRowsHeader: false
         };
-        let test_list = [];
-        for(let i=0; i<rowsDeleted.data.length; i++){
-            test_list.push(rowsDeleted.data[i]['dataIndex']);
-            console.log(test_list);
-        }
-        console.log("-----",rowsDeleted.data, "were deleted!");
-      },
-      onChangePage: (numberRows) => {
-        console.log(numberRows);
-      },
-      onSearchChange: (searchText) => {
-        console.log(searchText);
-      },
-      onColumnSortChange: (column, direction) => {
-        console.log(column, direction);
-      },
-      onColumnViewChange: (column, action) => {
-        console.log(column, action);
-      },
-      onFilterChange: (column, filters) => {
-        console.log(column, filters);
-      },
-      onCellClick: (cellData, cellMeta) => {
-        console.log(cellData, cellMeta);
-      },
-      onRowClick: (rowData, rowState) => {
-        console.log(rowData, rowState);
-      },
-      isRowSelectable: (dataIndex, selectedRows) => {
-        //prevents selection of any additional row after the third
-        if (selectedRows.data.length > 999 && selectedRows.data.filter(d => d.dataIndex === dataIndex).length === 0) return false;
-        //prevents selection of row with title "Attorney"
-        return data[dataIndex][1] != "Attorney";
-      },
-      selectableRowsHeader: false
-    };
 
 
         return (
@@ -103,16 +113,17 @@ class Board extends React.Component {
                 <div>
                     <BoardForm
                         onCreate={this.handleCreate}
-                        onDelete={this.handleDelete}
+                    // onDelete={this.handleDelete}
                     />
                 </div>
                 <MUIDataTable
+
                     title={"Test_Board"}
                     data={data}
                     columns={columns}
                     options={options}
                 />
-            </Fragment>
+            </Fragment >
         );
     }
 }
